9dba7d38c2f0ca447e8c5ced9682861cd44fcd80
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-09 10:52:47 +0300

    color game

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', app , false);
+
+function app (){
+
+    const rElement = document.getElementById("r");
+const gElement = document.getElementById("g");
+const bElement = document.getElementById("b");
+
+console.log(document.getElementsByClassName("mode"));
+
+}
+
+
